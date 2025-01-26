4ad910f9b01a79100e3c32aee0ee0466f003a6f0
goose <samokhinmarks@gmail.com>
2025-01-26 20:33:04 +0100

    before changing ngrok

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,10 +1,31 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 
 import "./Login.css"
 import { LogoIcon } from "../ icons/LogoIcon";
 
 
 export const Login: FC = () => {
+	const telegramBtnRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (document.querySelector("script[data-telegram-login]")) {
+      console.log("Telegram script is already loaded");
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.async = true;
+    script.setAttribute("data-telegram-login", "ChainStalkerBot");
+    script.setAttribute("data-size", "large");
+    script.setAttribute("data-user-pic", "false");
+    script.setAttribute("data-auth-url", "https://google.com");
+    
+    telegramBtnRef.current?.appendChild(script);
+    console.log("Telegram script loaded successfully");
+	}, []);
+
+
 	return (
 		<div className="window">
 			<div className="content">
@@ -14,9 +35,12 @@ export const Login: FC = () => {
 			<div className="descr">
 				<p className="text-normal">Manage your subscriptions</p>
 				<p className="text-small">Connect your telegram and manage your subscriptions easily</p>
-				<div className="btn glass_effect text-normal" style={{marginTop: "30px"}}>
+				<div ref={telegramBtnRef} className="btn glass_effect text-normal" 
+							style={{ marginTop: "30px" }}>
 					<span>Connect with telegram</span>
 				</div>
+
+				<noscript>You must enable javascript to connect your telegram</noscript>
 			</div>
 		</div>
 	);
