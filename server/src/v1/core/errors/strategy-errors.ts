c5961ed544eba3c48bb38f819ca248b3e8dc34b8
goose <samokhinmarks@gmail.com>
2025-03-17 16:37:32 +0000

    created: tests for AbsoluteStrategy

--- server/src/v1/core/errors/strategy-errors.ts
+++ server/src/v1/core/errors/strategy-errors.ts
@@ -1,18 +1,30 @@
 import { DomainError } from "./domain-error.abstract";
 
-export class InvalidStrategyError extends DomainError {
+export class StrategyError extends DomainError {
 	constructor(message: string) {
 		super(message);
 	}
 }
 
-export class InvalidStrategyConfigurationError extends DomainError {
-	constructor(strategy: string, reason: string) {
+export class StrategyConfigurationError extends DomainError {
+	constructor(readonly strategy: string, readonly reason: string) {
 		super(`Invalid configuration for strategy "${strategy}": ${reason}`);
 	}
 }
 
-export class InvalidPriceStrategyError extends DomainError {
+export class ThresholdStrategyConfigurationErrror extends StrategyConfigurationError {
+	constructor(strategy: string, reason: string) {
+		super(strategy, reason);
+	}
+}
+
+export class RangeStrategyConfigurationError extends StrategyConfigurationError {
+	constructor(strategy: string, reason: string) {
+		super(strategy, reason);
+	}
+}
+
+export class PriceStrategyTypeError extends DomainError {
 	constructor(type: string) {
 		super(`Unknown price strategy type: ${type}`);
 	}
--- server/src/v1/server.ts
+++ server/src/v1/server.ts
@@ -1,5 +1,7 @@
+import { PercentageChangeStrategy } from "#core/strategies/notification/index";
 import dotenv from "dotenv";
 dotenv.config();
 
 
 // startServer();
+new PercentageChangeStrategy(Infinity);
