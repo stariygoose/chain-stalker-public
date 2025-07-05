1b2e15ddc4b8221163f9c1baee8725ae1f72f07f
stariygoose <samokhinmarks@gmail.com>
2025-07-05 16:18:00 +0200

    logger daily file rotation for server and bot

--- bot/src/config/logger/logger.ts
+++ bot/src/config/logger/logger.ts
@@ -1,66 +1,93 @@
 import { inject, injectable } from "inversify";
 import winston, { createLogger, format } from "winston";
+import "winston-daily-rotate-file";
 
-import { IConfigService } from "#config/config/config.service.js";
+import { ConfigService } from "#config/config/config.service.js";
 import { TYPES } from "#di/types.js";
 
-
 export interface ILogger {
-	info(message: string): void;
-	debug(message: string): void;
-	warn(message: string): void;
-	error(message: string): void;
+  info(message: string): void;
+  debug(message: string): void;
+  warn(message: string): void;
+  error(message: string): void;
 }
 
 @injectable()
 export class Logger implements ILogger {
-	private readonly _logger: winston.Logger;
-	constructor (
-		@inject(TYPES.ConfigService)
-		private readonly _config: IConfigService
-	) {
-		this._logger = this.setupLogger();
-	}
+  private readonly _logger: winston.Logger;
 
-	public warn(message: string): void {
-		this._logger.warn(message);
-	}
-	
-	public error(message: string): void {
-		this._logger.error(message);
-	}
+  constructor(
+    @inject(TYPES.ConfigService)
+    private readonly _config: ConfigService,
+  ) {
+    this._logger = this.setupLogger();
+  }
 
-	public debug(message: string): void {
-		this._logger.debug(message);
-	}
+  public warn(message: string): void {
+    this._logger.warn(message);
+  }
 
-	public info(message: string): void {
-		this._logger.info(message);
-	}
+  public error(message: string): void {
+    this._logger.error(message);
+  }
 
-	private setupLogger(): winston.Logger {
-		return createLogger({
-			level: this.getLoggerLevel(),
-			format: format.combine(
-				format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
-				format.printf(({ level, message, timestamp }) => {
-					return `|${timestamp}| [${level}]: ${message}`
-			}),
-			),
-			transports: [
-				new winston.transports.Console({
-					format: format.combine(format.colorize(), format.printf(({ level, message, timestamp }) => {
-						return `|${timestamp}| [${level}]: ${message}`;
-					}))
-				}),
-				new winston.transports.File({ filename: './.logs/combined.log', format: format.uncolorize() }),
-				new winston.transports.File({ filename: './.logs/errors.log', format: format.uncolorize(), level: 'error' }),
-				new winston.transports.File({ filename: './.logs/warnings.log', format: format.uncolorize(), level: 'warn' }),
-			]
-		});
-	}
+  public debug(message: string): void {
+    this._logger.debug(message);
+  }
 
-	private getLoggerLevel(): 'debug' | 'info' {
-		return this._config.isDevMode ? 'debug' : 'info';
-	}
+  public info(message: string): void {
+    this._logger.info(message);
+  }
+
+  private setupLogger(): winston.Logger {
+    return createLogger({
+      level: this.getLoggerLevel(),
+      format: format.combine(
+        format.timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
+        format.printf(({ level, message, timestamp }) => {
+          return `|${timestamp}| [${level}]: ${message}`;
+        }),
+      ),
+      transports: [
+        new winston.transports.Console({
+          format: format.combine(
+            format.colorize(),
+            format.printf(({ level, message, timestamp }) => {
+              return `|${timestamp}| [${level}]: ${message}`;
+            }),
+          ),
+        }),
+        this.rotate("error", false),
+        this.rotate("warn", false),
+        this.rotate("info", false),
+      ],
+    });
+  }
+
+  private getLoggerLevel(): "debug" | "info" {
+    return this._config.isDevMode ? "debug" : "info";
+  }
+
+  private rotate(level: "error" | "warn" | "info", colorize: boolean) {
+    const dailyTransport = new winston.transports.DailyRotateFile({
+      level: level,
+      dirname: "./.logs/",
+      format: colorize ? format.colorize() : format.uncolorize(),
+      filename: "%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+    });
+
+    const emergency = createLogger({
+      transports: [new winston.transports.Console({ stderrLevels: ["error"] })],
+    });
+
+    dailyTransport.on("error", (err) => {
+      emergency.error(`[LOGGER ERROR]: ${err.message}`);
+    });
+
+    return dailyTransport;
+  }
 }
